477e1ae4ef2a9f5b247b173799c3715de2099c6a
bocon <tan.truong@go2joy.vn>
2021-07-28 22:15:06 +0700

    Add - TH02 - BT02

---
+++ src/ThucHanh/TH02/BT02/Sinhvien.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "List.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Sinhvien {
+    string ten;
+    string mssv;
+    float dtb;
+};
---
+++ src/ThucHanh/TH02/BT02/main.cpp
@@ -0,0 +1,11 @@
+#include "ListSV.h"
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    LIST DanhSachSV;
+    return 1;
+}
